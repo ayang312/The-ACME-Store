dd0cf2d4a1dc5cf3c59702cbc77e76edbb203f7c
Andrew Yang <ayang312@gmail.com>
2024-11-19 21:29:02 -0500

    completed functions

--- server/db.js
+++ server/db.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const pg = require("pg");
 // setup server
 const client = new pg.Client(
@@ -9,31 +8,112 @@ const client = new pg.Client(
 const uuid = require("uuid");
 
 const createTables = async () => {
-    const SQL = /* sql */ `
+  const SQL = /* sql */ `
         DROP TABLE IF EXISTS users;
         DROP TABLE IF EXISTS products;
         DROP TABLE IF EXISTS favorites;
 
-        CREATE TABLE users;
+        CREATE TABLE users(
         id UUID PRIMARY KEY,
         username VARCHAR(20) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL
+    );
 
-        CREATE TABLE products;
+        CREATE TABLE products(
         id UUID PRIMARY KEY,
-        name VARCHAR(100) NOT NULL UNIQUE,
+        name VARCHAR(100) NOT NULL UNIQUE
+    );
 
-        CREATE TABLE favorites;
+        CREATE TABLE favorites(
         id UUID PRIMARY KEY,
         product_id UUID REFERENCES products(id) NOT NULL,
         user_id UUID REFERENCES users(id) NOT NULL,
         CONSTRAINT unique_product_user UNIQUE (product_id, user_id)
+    );
     `;
-    await client.query(SQL);
+  await client.query(SQL);
 };
 
+// createUser async fxn
+const createUser = async ({ username, password }) => {
+  const SQL = /*sql*/ `
+        INSERT INTO users(id, username, password) 
+        VALUES($1, $2, $3) 
+        RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid4(), username, password]);
+  return response.rows[0];
+};
 
+// createProduct async fxn
+const createProduct = async ({ name }) => {
+  const SQL = /* sql */ `
+        INSERT INTO products(id, name)
+        VALUES($1, $2)
+        RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid4(), name]);
+  return response.rows[0];
+};
+
+// fetchUsers fxn
+const fetchUsers = async () => {
+  const SQL = /* sql */ `
+        SELECT id, username, FROM users
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+// fetchProducts fxn
+const fetchProducts = async () => {
+  const SQL = /* sql */ `
+        SELECT * FROM products
+    `;
+  const response = await client.query(SQL);
+  return response.rows[0];
+};
+
+// createFavorite fxn
+const createFavorite = async ({ product_id, user_id }) => {
+  const SQL = /* sql */ `
+        INSERT INTO favorites(id, product_id, user_id)
+        VALUES($1, $2, $3)
+        RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid4(), product_id, user_id]);
+  return response.rows[0];
+};
+
+// fetchFavorites fxn
+const fetchFavorites = async (user_id) => {
+  const SQL = /* sql */ `
+        SELECT *
+        FROM favorites
+        WHERE user_id = $1
+    `;
+  const response = await client.query(SQL, [user_id]);
+  return response.rows;
+};
+
+// destroyFavorite fxn
+const destroyFavorite = async ({ id, user_id }) => {
+  const SQL = /* sql */ `
+        DELETE
+        FROM favorites
+        WHERE id = $1 and user_id = $2
+    `;
+  await client.query(SQL, [id, user_id]);
+};
 
 module.exports = {
-    client
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  fetchUsers,
+  fetchProducts,
+  createFavorite,
+  fetchFavorites,
+  destroyFavorite,
 };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,58 @@
-const { client } = require("./db");
+const {
+  client,
+  createTables,
+  createUser,
+  fetchUsers,
+  fetchProducts,
+  fetchFavorites,
+  createFavorite,
+  destroyFavorite,
+  createProduct,
+} = require("./db");
+
+const express = require("express");
+const app = express();
+
+
 
 // init function to connect to database
-const init = async() => {
-    await client.connect();
-    console.log('connected to database');
+const init = async () => {
+  await client.connect();
+  console.log("connected to database");
+
+  await createTables();
+  console.log("tables created");
+
+  const [andrew, kathy, maui, bowlingBall, camera, palette, lipStick, bone] =
+    await Promise.all([
+      createUser({ username: "andrew", password: "pw1" }),
+      createUser({ username: "kathy", password: "pw2" }),
+      createUser({ username: "maui", password: "pw3" }),
+      createProduct({ name: "bowlingBall" }),
+      createProduct({ name: "camera" }),
+      createProduct({ name: "palette" }),
+      createProduct({ name: "lipStick" }),
+      createProduct({ name: "bone" }),
+    ]);
+
+    const createProducts = await Promise.all([
+        createFavorite({user_id: andrew.id, product_id: bowlingBall.id}),
+        createFavorite({user_id: andrew.id, product_id: camera.id}),
+        createFavorite({user_id: kathy.id, product_id: palette.id}),
+        createFavorite({user_id: kathy.id, product_id: lipStick.id}),
+        createFavorite({user_id: maui.id, product_id: bone.id}),
+        createFavorite({user_id: maui.id, product_id: camera.id}),
+    ]);
+    console.log("data seeded");
+
+    // get products
+
+    // create favorites
+
+    // delete favorites
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
 };
 
 init();
