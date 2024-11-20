3a99662982b561ece5b76b74a5781d0b65d26859
Andrew Yang <ayang312@gmail.com>
2024-11-19 20:51:29 -0500

    created tables and constraint

--- server/db.js
+++ server/db.js
@@ -1,7 +1,38 @@
+require("dotenv").config();
 const pg = require("pg");
+// setup server
 const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/the_acme_store_db"
 );
+
+// unique keys
+const uuid = require("uuid");
+
+const createTables = async () => {
+    const SQL = /* sql */ `
+        DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS products;
+        DROP TABLE IF EXISTS favorites;
+
+        CREATE TABLE users;
+        id UUID PRIMARY KEY,
+        username VARCHAR(20) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+
+        CREATE TABLE products;
+        id UUID PRIMARY KEY,
+        name VARCHAR(100) NOT NULL UNIQUE,
+
+        CREATE TABLE favorites;
+        id UUID PRIMARY KEY,
+        product_id UUID REFERENCES products(id) NOT NULL,
+        user_id UUID REFERENCES users(id) NOT NULL,
+        CONSTRAINT unique_product_user UNIQUE (product_id, user_id)
+    `;
+    await client.query(SQL);
+};
+
+
 
 module.exports = {
     client
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const { client } = require("./db");
 
+// init function to connect to database
 const init = async() => {
     await client.connect();
     console.log('connected to database');
